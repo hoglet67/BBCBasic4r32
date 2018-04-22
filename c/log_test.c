e54364fdb20cd6a07f8d7e4e6497200def828434
David Banks <dave@hoglet.com>
2018-04-22 16:09:02 +0100

    Updated log_test to use long double

--- c/log_test.c
+++ c/log_test.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
+#define DOUBLE long double
+
 // The range to test over
 #define  MIN (0.5 * sqrt(2.0))
 #define  MAX (1.0 * sqrt(2.0))
 #define STEP ((MAX - MIN) / 1000.0)
 
-typedef double (*function)(double, int);
+typedef DOUBLE (*function)(DOUBLE, int);
 
 // The simplest series for log
 //
@@ -15,14 +17,14 @@ typedef double (*function)(double, int);
 //                           2     3     4
 // x = 1 + z and z = x - 1
 
-double taylor_series_log(double x, int nterms) {
+DOUBLE taylor_series_log(DOUBLE x, int nterms) {
    int i;
-   double z = x - 1.0;
-   double result = 0.0;
-   double sign = 1;
+   DOUBLE z = x - 1.0;
+   DOUBLE result = 0.0;
+   DOUBLE sign = 1;
    for (i = 0; i < nterms; i++) {
-      double p = i + 1;
-      result += sign * pow(z, p) / p;
+      DOUBLE p = i + 1;
+      result += sign * powl(z, p) / p;
       sign = -sign;
    }
    return result;
@@ -38,13 +40,13 @@ double taylor_series_log(double x, int nterms) {
 // x = ----- and  z = -----
 //     1 + z          1 + x
 
-double taylor_odd_series_log(double x, int nterms) {
+DOUBLE taylor_odd_series_log(DOUBLE x, int nterms) {
    int i;
-   double z = (1.0 - x) / (1.0 + x);
-   double result = 0.0;
+   DOUBLE z = (1.0 - x) / (1.0 + x);
+   DOUBLE result = 0.0;
    for (i = 0; i < nterms; i++) {
-      double p = 1 + 2 * i;
-      result += pow(z, p) / p;
+      DOUBLE p = 1 + 2 * i;
+      result += powl(z, p) / p;
    }
    result *= -2.0;
    return result;
@@ -53,11 +55,11 @@ double taylor_odd_series_log(double x, int nterms) {
 //
 // Follows the implementation in BBC Basic 4r32
 //
-double bbcbasic4r32_log(double x, int nterms) {
-   const double C3 = -1.533333333;
-   const double C4 = -0.617142857;
-   const double C5 = -1.800000000;
-   double b, c, d, e, f, g, h, i, j, k;
+DOUBLE bbcbasic4r32_log(DOUBLE x, int nterms) {
+   const DOUBLE C3 = -1.533333333;
+   const DOUBLE C4 = -0.617142857;
+   const DOUBLE C5 = -1.800000000;
+   DOUBLE b, c, d, e, f, g, h, i, j, k;
    b = (1.0 - x) / (1.0 + x);
    c = 3.0 / (b * b);
    int use_extra = (nterms > 1) || (nterms == 0 && c > 1.21307373E-3);
@@ -76,11 +78,11 @@ double bbcbasic4r32_log(double x, int nterms) {
    return k;
 }
 
-double evaluate_error(function f, int nterms) {
-   double max_err = -1.0;
-   for (double x = MIN; x < MAX; x += STEP) {
-      double val = f(x, nterms);
-      double err = fabs(val - log(x));
+DOUBLE evaluate_error(function f, int nterms) {
+   DOUBLE max_err = -1.0;
+   for (DOUBLE x = MIN; x < MAX; x += STEP) {
+      DOUBLE val = f(x, nterms);
+      DOUBLE err = fabsl(val - logl(x));
       if (err > max_err) {
          max_err = err;
       }
@@ -90,22 +92,22 @@ double evaluate_error(function f, int nterms) {
 
 int main() {
    for (int nterms = 1; nterms < 20; nterms ++) {
-      printf("Taylor series A with %3d terms: max_err = %e\n",
+      printf("Taylor series A with %3d terms: max_err = %Le\n",
              nterms, evaluate_error(taylor_series_log, nterms));
    }
    printf("\n");
    for (int nterms = 1; nterms < 20; nterms ++) {
-      printf("Taylor series B with %3d terms: max_err = %e\n",
+      printf("Taylor series B with %3d terms: max_err = %Le\n",
              nterms, evaluate_error(taylor_odd_series_log, nterms));
    }
    printf("\n");
-   printf("BBC Basic 4r32 with 1   coeffs: max_err = %e\n",
+   printf("BBC Basic 4r32 with 1   coeffs: max_err = %Le\n",
           evaluate_error(bbcbasic4r32_log, 1));
 
-   printf("BBC Basic 4r32 with 3   coeffs: max_err = %e\n",
+   printf("BBC Basic 4r32 with 3   coeffs: max_err = %Le\n",
           evaluate_error(bbcbasic4r32_log, 3));
 
-   printf("BBC Basic 4r32 with 1/3 coeffs: max_err = %e\n",
+   printf("BBC Basic 4r32 with 1/3 coeffs: max_err = %Le\n",
           evaluate_error(bbcbasic4r32_log, 0));
    printf("\n");
 }
